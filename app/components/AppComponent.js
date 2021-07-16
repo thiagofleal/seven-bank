50f2c0aa0113a7cec76e086ef01b75600d693ef8
Thiago Fernandes Leal <thiagofernandesleal@yahoo.com.br>
2021-07-15 21:54:16 -0300

    Corrigir verificação de permissões

--- app/components/AppComponent.js
+++ app/components/AppComponent.js
@@ -44,7 +44,7 @@ export default class AppComponent extends Component
 	onFirst() {
 		const items = [];
 
-		if (this.auth.hasPermission('VIEW')) {
+		if (this.auth.hasPermission('ACC')) {
 			items.push({
 				id: 'menu-account',
 				icon: "fa fa-user",
@@ -52,7 +52,7 @@ export default class AppComponent extends Component
 				href: "#"
 			});
 		}
-		if (this.auth.hasPermission('TRANSFER')) {
+		if (this.auth.hasPermission('TRANSF')) {
 			items.push({
 				id: 'menu-transfer',
 				icon: "fa fa-dollar",
@@ -60,12 +60,12 @@ export default class AppComponent extends Component
 				href: "#/transferir"
 			});
 		}
-		if (this.auth.hasPermission('CREATE')) {
+		if (this.auth.hasPermission('ADM_ACC')) {
 			items.push({
 				id: 'menu-manage',
-				icon: "fa fa-plus",
-				title: "Gerenciar contas",
-				href: "#/transferir"
+				icon: "fa fa-users",
+				title: "Contas",
+				href: "#/contas"
 			});
 		}
 		items.push({
